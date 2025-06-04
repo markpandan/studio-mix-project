d0d9d5975a4db2bbab134d5e5ab5cf3988d7ac1d
markpandan <72681401+markpandan@users.noreply.github.com>
2025-06-04 23:27:54 +0800

    Remove unused props and imports

--- src/components/Carousel/CarouselDescriptions.tsx
+++ src/components/Carousel/CarouselDescriptions.tsx
@@ -1,39 +1,40 @@
-import { VStack, Heading, Text } from "@chakra-ui/react";
+import { VStack, Text } from "@chakra-ui/react";
 import { projectColorTertiary } from "../../theme";
 import CarouselIndicators from "./CarouselIndicators";
 
 interface Props {
-   activeIndex: number;
-   size: number;
-   handleClick: (index: number) => void;
+  activeIndex: number;
+  size: number;
+  handleClick: (index: number) => void;
 }
 
 const CarouselDescriptions = ({ activeIndex, size, handleClick }: Props) => {
-   return (
-      <VStack
-         borderTop={"2px solid"}
-         borderColor={projectColorTertiary}
-         alignItems={"start"}
-         backgroundColor={"blackAlpha.400"}
-         color={"gray.300"}
-         paddingX={10}
-         gap={1}
-         zIndex={2}
-      >
-         <Text fontSize={"2xl"} fontWeight={"800"}>
-            Lorem Ipsum
-         </Text>
-         <Text fontSize={"md"}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-            incididunt ut labore et dolore magna aliqua. Potenti nullam ac tortor
-         </Text>
-         <CarouselIndicators
-            select={activeIndex}
-            size={size}
-            onClick={(index) => handleClick(index)}
-         ></CarouselIndicators>
-      </VStack>
-   );
+  return (
+    <VStack
+      borderTop={"2px solid"}
+      borderColor={projectColorTertiary}
+      alignItems={"start"}
+      backgroundColor={"blackAlpha.400"}
+      color={"gray.300"}
+      paddingX={10}
+      gap={1}
+      zIndex={2}
+    >
+      <Text fontSize={"2xl"} fontWeight={"800"}>
+        Lorem Ipsum
+      </Text>
+      <Text fontSize={"md"}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Potenti nullam ac
+        tortor
+      </Text>
+      <CarouselIndicators
+        select={activeIndex}
+        size={size}
+        onClick={(index) => handleClick(index)}
+      ></CarouselIndicators>
+    </VStack>
+  );
 };
 
 export default CarouselDescriptions;
--- src/pages/Inquire.tsx
+++ src/pages/Inquire.tsx
@@ -1,64 +1,65 @@
-import { Box, HStack, Heading, Icon, SimpleGrid, Text, VStack } from "@chakra-ui/react";
+import { Box, Heading, Icon, SimpleGrid, Text, VStack } from "@chakra-ui/react";
 import { BsGeoAlt } from "react-icons/bs";
 import SubJumbotron from "../components/Jumbotrons/SubJumbotron";
 import {
-   defaultContainerSize,
-   projectColorPrimary,
-   projectColorSecondary,
-   projectColorTertiary,
-   jumbotronBackground_1,
+  defaultContainerSize,
+  projectColorPrimary,
+  projectColorSecondary,
+  projectColorTertiary,
+  jumbotronBackground_1,
 } from "../theme";
 import { BsEnvelope, BsCalendar2Check } from "react-icons/bs";
 
 // TODO: Make a Placeholder first while the iframe is not avaialable yet
 const Inquire = () => {
-   return (
-      <>
-         <SubJumbotron image={jumbotronBackground_1}>INQUIRE</SubJumbotron>
-         <Box bgColor={projectColorPrimary}>
-            <VStack spacing={5} maxW={defaultContainerSize} margin={"auto"} py={5}>
-               <Heading color={projectColorSecondary} alignSelf={"start"}>
-                  OUR LOCATION
-               </Heading>
-               <iframe
-                  src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d1735.8236047097416!2d121.0188298301581!3d14.465638946133565!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3397ce4680636c11%3A0xf9fe9f09c53665b8!2s110%20Sierra%2C%20San%20Antonio%2C%20Para%C3%B1aque%2C%201715%20Metro%20Manila!5e1!3m2!1sen!2sph!4v1710058552449!5m2!1sen!2sph"
-                  width={"100%"}
-                  height="450"
-                  style={{ border: 0 }}
-                  allowFullScreen={true}
-                  loading="lazy"
-                  referrerPolicy="no-referrer-when-downgrade"
-               />
-               <SimpleGrid columns={[1, 2]} width={"100%"}>
-                  <VStack alignItems={"start"}>
-                     <Heading size="md" color={projectColorSecondary}>
-                        ADDRESS:
-                     </Heading>
-                     <Text>
-                        <Icon as={BsGeoAlt} color={projectColorTertiary} /> Dr Arcadio Santos Ave,
-                        San Antonio, Parañaque, 1700 Metro Manila
-                     </Text>
-                     <Heading size="md" color={projectColorSecondary}>
-                        STORE HOURS:
-                     </Heading>
-                     <Text>
-                        <Icon as={BsCalendar2Check} color={projectColorTertiary} /> We are open from
-                        Monday to Friday, 9:00 AM to 5:00 PM.
-                     </Text>
-                  </VStack>
-                  <VStack alignItems={"start"}>
-                     <Heading size="md" color={projectColorSecondary}>
-                        CONTACT US AT:
-                     </Heading>
-                     <Text>
-                        <Icon as={BsEnvelope} color={projectColorTertiary} /> studiomix1@gmail.com
-                     </Text>
-                  </VStack>
-               </SimpleGrid>
+  return (
+    <>
+      <SubJumbotron image={jumbotronBackground_1}>INQUIRE</SubJumbotron>
+      <Box bgColor={projectColorPrimary}>
+        <VStack spacing={5} maxW={defaultContainerSize} margin={"auto"} py={5}>
+          <Heading color={projectColorSecondary} alignSelf={"start"}>
+            OUR LOCATION
+          </Heading>
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d1735.8236047097416!2d121.0188298301581!3d14.465638946133565!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3397ce4680636c11%3A0xf9fe9f09c53665b8!2s110%20Sierra%2C%20San%20Antonio%2C%20Para%C3%B1aque%2C%201715%20Metro%20Manila!5e1!3m2!1sen!2sph!4v1710058552449!5m2!1sen!2sph"
+            width={"100%"}
+            height="450"
+            style={{ border: 0 }}
+            allowFullScreen={true}
+            loading="lazy"
+            referrerPolicy="no-referrer-when-downgrade"
+          />
+          <SimpleGrid columns={[1, 2]} width={"100%"}>
+            <VStack alignItems={"start"}>
+              <Heading size="md" color={projectColorSecondary}>
+                ADDRESS:
+              </Heading>
+              <Text>
+                <Icon as={BsGeoAlt} color={projectColorTertiary} /> Dr Arcadio
+                Santos Ave, San Antonio, Parañaque, 1700 Metro Manila
+              </Text>
+              <Heading size="md" color={projectColorSecondary}>
+                STORE HOURS:
+              </Heading>
+              <Text>
+                <Icon as={BsCalendar2Check} color={projectColorTertiary} /> We
+                are open from Monday to Friday, 9:00 AM to 5:00 PM.
+              </Text>
             </VStack>
-         </Box>
-      </>
-   );
+            <VStack alignItems={"start"}>
+              <Heading size="md" color={projectColorSecondary}>
+                CONTACT US AT:
+              </Heading>
+              <Text>
+                <Icon as={BsEnvelope} color={projectColorTertiary} />{" "}
+                studiomix1@gmail.com
+              </Text>
+            </VStack>
+          </SimpleGrid>
+        </VStack>
+      </Box>
+    </>
+  );
 };
 
 export default Inquire;
